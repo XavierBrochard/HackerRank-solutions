e63f9a19261d1208eec84cc8c1fa0cd81569e3f6
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2020-09-20 14:32:05 +0200

    Document the tricky part

--- DataStructures/SparseArrays/Solution.js
+++ DataStructures/SparseArrays/Solution.js
@@ -2,7 +2,9 @@
 function matchingStrings(strings, queries) {
     const map = {};
     queries.forEach((q,i) => strings.forEach(s => {
-        map[q+i] = map[q+i] || 0;
+        // We compute a hash key that uses the query and its index 
+        // so that the keys do not collide when we have duplicate queries
+        map[q+i] = map[q+i] || 0; 
         if(q === s) map[q+i]++;
     }))
     console.log(map)
