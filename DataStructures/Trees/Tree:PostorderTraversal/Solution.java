e69722aae5b0560c13bb4839c254c33a871c469a
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2020-09-25 15:46:22 +0200

    🆕 Tree: Postorder Traversal

---
+++ DataStructures/Trees/Tree:PostorderTraversal/Solution.java
@@ -0,0 +1,23 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+public static void postOrder(Node root) {
+    Node cur = root;
+    if(cur.left != null) {
+        postOrder(cur.left);
+    } 
+    if(cur.right != null) {
+        postOrder(cur.right);
+    }
+    System.out.print(cur.data + " ");
+}
